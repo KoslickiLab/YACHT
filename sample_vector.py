b9d6a7b92c8ae3e4120b0f337bf114bc48e5f9da
David Koslicki <dmkoslicki@gmail.com>
2022-10-18 17:19:47 -0400

    Start adding doc strings

--- sample_vector.py
+++ sample_vector.py
@@ -4,6 +4,15 @@ import utils
 
 
 def sample_vector_from_signature(signature, hash_to_idx):
+    """
+    Given a signature and a dictionary mapping hashes to indices, return a vector whose basis is indexed by
+    all hashes seen in the training dictionary. The value at each index is the abundance of the hash in the
+    signature.
+
+    :param signature: a sourmash minhash object
+    :param hash_to_idx: a dictionary mapping hashes of the training dictionary to indices
+    :return: numpy vector
+    """
     K = len(list(hash_to_idx.keys()))
     sample_vec = np.zeros(K)
     sig_hashes = signature.minhash.hashes
@@ -15,6 +24,14 @@ def sample_vector_from_signature(signature, hash_to_idx):
 
 
 def sample_vector_from_files(sig_filename, hash_filename, ksize):
+    """
+    Helper function to load a signature and a hash_to_idx dictionary from files and return a sample vector.
+
+    :param sig_filename: filename of the sourmash signature
+    :param hash_filename: filename of the hash_to_col_idx.csv file which maps hashes to indices
+    :param ksize: ksize of the signature
+    :return: numpy vector (sample vector y)
+    """
     sample_sig = utils.load_signature_with_ksize(sig_filename, ksize)
     hash_to_idx = utils.load_hashes(hash_filename)
     return sample_vector_from_signature(sample_sig, hash_to_idx)
--- utils.py
+++ utils.py
@@ -4,10 +4,16 @@ import sourmash
 
 
 def load_hashes(filename):
+    """
+    Helper function that loads the hash_to_col_idx.csv file and returns a dictionary mapping hashes to indices in the
+    training dictionary. filename should point to a CSV file with two columns: hash, col_idx.
+    :param filename: string (location of the hash_to_col_idx.csv file)
+    :return: dictionary mapping hashes to indicies
+    """
     with open(filename, mode='r') as infile:
         next(infile)
         reader = csv.reader(infile)
-        hashes = {int(rows[0]):int(rows[1]) for rows in reader}
+        hashes = {int(rows[0]): int(rows[1]) for rows in reader}
     return hashes
 
 
@@ -25,7 +31,8 @@ def load_signature_with_ksize(filename, ksize):
         if sig.minhash.ksize == ksize:
             return sig
     raise ValueError(f'File {filename} does not contain sketch for ksize = {ksize}.')
-    
+
+
 def signatures_mismatch_ksize(signatures, ksize):
     for sig in signatures:
         if sig.minhash.ksize != ksize:
@@ -34,12 +41,12 @@ def signatures_mismatch_ksize(signatures, ksize):
 
 
 #crude monte carlo method, to be replaced
-def estimate_w_temp(k, num_hashes, mut_thresh = 0.05, est_n_orgs = 1000, p_val = 0.99, n_tests = 10000):
-    prob = (1-mut_thresh)**k
+def estimate_w_temp(k, num_hashes, mut_thresh=0.05, est_n_orgs=1000, p_val=0.99, n_tests=10000):
+    prob = (1 - mut_thresh) ** k
     b = []
     for i in range(n_tests):
         b.append(min(np.random.binomial(num_hashes, prob, (est_n_orgs, 1))))
-    min_est = np.quantile(b,p_val)
+    min_est = np.quantile(b, p_val)
     print(min_est)
-    w = min_est/(num_hashes-min_est)
+    w = min_est / (num_hashes - min_est)
     return w
