6dd4fdf888b90600a9af789ad49985f2dcdaa980
dkoslicki <dmkoslicki@gmail.com>
2022-11-10 15:36:23 -0500

    update scripts for #5

--- experiments/download_db.sh
+++ experiments/download_db.sh
@@ -1,3 +1,3 @@
-python ../../KEGG_sketching_annotation/scripts/get_reference_genomes.py -n 100 -u -s ref_genomes_3 -S 2 -c sew347@psu.edu
+python ../../KEGG_sketching_annotation/scripts/get_reference_genomes.py -n 200 -u -s ref_genomes_3 -S 22 -c anonymous
 
-python ../../KEGG_sketching_annotation/scripts/create_genome_ref_db.py ./ref_genomes_3/reference_genomes 100_database 100
+#python ../../KEGG_sketching_annotation/scripts/create_genome_ref_db.py ./ref_genomes_3/reference_genomes 100_database 100
--- experiments/run_methods.sh
+++ experiments/run_methods.sh
@@ -4,9 +4,9 @@ set -u
 set -o pipefail
 
 # Make the sourmash sketch
-sourmash sketch dna -f -p k=31,scaled=1000,abund -o simulated_mg.fq.sig simulated_mg.fq
+#sourmash sketch dna -f -p k=31,scaled=1000,abund -o simulated_mg_10M.fq.sig simulated_mg_10M.fq
 # Run gather
-sourmash gather --dna --threshold-bp 100 simulated_mg.fq.sig without_unknown_db.sig -o gather_results.csv
+sourmash gather --dna --threshold-bp 100 simulated_mg_10M.fq.sig without_unknown_db.sig -o gather_results_10M.csv
 
 # then run our approach
-python ../../ref_matrix.py --ref_file without_unknown_db.sig  --ksize 31 --out_prefix default_EU
+python ../../recover_abundance.py --ref_file default_EUref_matrix_processed.npz  --ksize 31 --sample_file simulated_mg_10M.fq.sig --outfile EU_results_default_10M.csv
