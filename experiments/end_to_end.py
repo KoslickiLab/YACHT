c90fe00585484ae33017c4ba0c94b2bc7183478f
dkoslicki <dmkoslicki@gmail.com>
2022-11-10 15:55:49 -0500

    only make the reference if it doesn't already exist. Update script for #5

--- experiments/end_to_end.py
+++ experiments/end_to_end.py
@@ -4,20 +4,23 @@ set -u
 set -o pipefail
 # This first command need only be run once
 echo "Making database"
-python ../../KEGG_sketching_annotation/scripts/create_genome_ref_db.py ./ref_genomes_3/reference_genomes 1046_database 1046
+#python ../../KEGG_sketching_annotation/scripts/create_genome_ref_db.py ./ref_genomes_3/reference_genomes 1046_database 1046
 
 simsFolder=sims-$(date +"%s")
 mkdir ${simsFolder}
 echo "Creating simulation"
-python run_sim.py --genomes_folder ref_genomes_3/reference_genomes --out_folder ${simsFolder} --num_reads 10000000
+ln -s formatted_db.fasta ${simsFolder}/formatted_db.fasta
+python run_sim.py --genomes_folder ref_genomes_3/reference_genomes --out_folder ${simsFolder} --num_reads 10000 --num_orgs 1000
 
+
+# Note: I could do a loop here if I felt like it
 N=500
 # remove N genomes from the training datbase
 echo "Making ${N} genomes unkown"
 cat ${simsFolder}/simulation_counts.csv | shuf | head -n ${N} | cut -d',' -f1 > ${simsFolder}/unknown_names.txt
 # remove those from the training datbase
 echo "Removing them from the ref db"
-../../../KEGG_sketching_annotation/utils/bbmap/./filterbyname.sh in=${simsFolder}/formatted_db.fasta out=${simsFolder}/without_unknown_db.fasta names=${simsFolder}unknown_names.txt include=f
+../../KEGG_sketching_annotation/utils/bbmap/./filterbyname.sh in=${simsFolder}/formatted_db.fasta out=${simsFolder}/without_unknown_db.fasta names=${simsFolder}/unknown_names.txt include=f overwrite=true
 # Sketching the reference 
 echo "Sketching the reference"
 sourmash sketch dna -f -p k=31,scaled=1000,abund -o ${simsFolder}/without_unknown_db.sig --singleton ${simsFolder}/without_unknown_db.fasta
--- experiments/run_sim.py
+++ experiments/run_sim.py
@@ -74,6 +74,7 @@ if __name__ == "__main__":
     parser.add_argument('--genomes_folder', help='Location of genome subfolders', required=True)
     parser.add_argument('--out_folder', help='Destination for output files', required=True)
     parser.add_argument('--num_reads', type=int, default=10**6, help='Number of reads to simulate', required=False)
+    parser.add_argument('--num_orgs', type=int, default=250, help='Number of organisms to include in the simulation (approximate)', required=False)
     args = parser.parse_args()
     
     if not os.path.exists(args.out_folder):
@@ -82,7 +83,7 @@ if __name__ == "__main__":
     db_filename = args.out_folder + '/formatted_db.fasta'
     mg_filename = args.out_folder + '/simulated_mg.fq'
     count_filename =  args.out_folder + '/simulation_counts.csv'
-
-    format_db_file(args.genomes_folder, db_filename)
-    KSA.run_simulation(db_filename, mg_filename, args.num_reads)
+    if not os.path.exists(db_filename):
+        format_db_file(args.genomes_folder, db_filename)
+    KSA.run_simulation(db_filename, mg_filename, args.num_reads, num_orgs=args.num_orgs, len_reads=150, noisy=False)
     parse_sim(mg_filename, count_filename)
