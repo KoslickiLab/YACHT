d6306aa2988cdda9b2c90ab8309fa940bb9637fb
dkoslicki <dmkoslicki@gmail.com>
2022-11-11 13:47:24 -0500

    fix occasional bc syntax error #5

--- experiments/end_to_end.sh
+++ experiments/end_to_end.sh
@@ -13,47 +13,43 @@ mkdir ${simsFolder}
 echo "Creating simulation"
 cd ${simsFolder}
 ln -s ../formatted_db.fasta formatted_db.fasta
-cd ..
-python run_sim.py --genomes_folder ref_genomes_3/reference_genomes --out_folder ${simsFolder} --num_reads ${numReads} --num_orgs 1000
-
+python ../run_sim.py --genomes_folder ../ref_genomes_3/reference_genomes --out_folder . --num_reads ${numReads} --num_orgs 500
+echo "sketching the simulation"
+sourmash sketch dna -f -p k=31,scaled=1000,abund -o simulated_mg.fq.sig simulated_mg.fq
+rm simulated_mg.fq
+rm -rf ref
 
 # Note: I could do a loop here if I felt like it
-N=500
+N=50
 # remove N genomes from the training datbase
 echo "Making ${N} genomes unkown"
-cat ${simsFolder}/simulation_counts.csv | shuf | head -n ${N} | cut -d',' -f1 | sort > ${simsFolder}/unknown_names.txt
-cat ${simsFolder}/simulation_counts.csv | cut -d',' -f1 | sort > ${simsFolder}/all_names.txt
-comm -2 -3 ${simsFolder}/all_names.txt ${simsFolder}/unknown_names.txt > ${simsFolder}/known_names.txt
-sed -n 2p MANIFEST.csv > ${simsFolder}/known_names_picklist.txt
-cat ${simsFolder}/known_names.txt |cut -d'.' -f1 | xargs -I{} grep {} MANIFEST.csv >> ${simsFolder}/known_names_picklist.txt
-#echo "name" > ${simsFolder}/known_names_picklist.txt
-#cat ${simsFolder}/known_names.txt >> ${simsFolder}/known_names_picklist.txt
+cat simulation_counts.csv | shuf | head -n ${N} | cut -d',' -f1 | sort > unknown_names.txt
+cat simulation_counts.csv | cut -d',' -f1 | sort > all_names.txt
+comm -2 -3 all_names.txt unknown_names.txt > known_names.txt
+sed -n 2p ../MANIFEST.csv > known_names_picklist.txt
+cat known_names.txt | cut -d'.' -f1 | xargs -I{} grep {} ../MANIFEST.csv >> known_names_picklist.txt
 
 # remove those from the training datbase
 echo "Removing them from the ref db"
-#../../KEGG_sketching_annotation/utils/bbmap/./filterbyname.sh in=${simsFolder}/formatted_db.fasta out=${simsFolder}/without_unknown_db.fasta names=${simsFolder}/unknown_names.txt include=f overwrite=true
 # Sketching the reference 
 echo "Sketching the reference"
-#sourmash sketch dna -f -p k=31,scaled=1000,abund -o ${simsFolder}/without_unknown_db.sig --singleton ${simsFolder}/without_unknown_db.fasta
-sourmash sig extract --picklist ${simsFolder}/known_names_picklist.txt:md5:md5 formatted_db.sig -o ${simsFolder}/without_unknown_db.sig 
+sourmash sig extract --picklist known_names_picklist.txt:md5:md5 ../formatted_db.sig -o without_unknown_db.sig 
 echo "Making the EU dictionary"
-python ../ref_matrix.py --ref_file ${simsFolder}/without_unknown_db.sig  --ksize 31 --out_prefix ${simsFolder}/default_EU_
+python ../../ref_matrix.py --ref_file without_unknown_db.sig  --ksize 31 --out_prefix default_EU_
 # then run the methods
-echo "sketching the simulation"
-sourmash sketch dna -f -p k=31,scaled=1000,abund -o ${simsFolder}/simulated_mg.fq.sig ${simsFolder}/simulated_mg.fq
 # Run gather
 echo "running gather"
-sourmash gather --dna --threshold-bp 100 ${simsFolder}/simulated_mg.fq.sig ${simsFolder}/without_unknown_db.sig -o ${simsFolder}/gather_results.csv
+sourmash gather --dna --threshold-bp 100 simulated_mg.fq.sig without_unknown_db.sig -o gather_results.csv
 
 # then run our approach
-python ../recover_abundance.py --ref_file ${simsFolder}/default_EU_ref_matrix_processed.npz  --ksize 31 --sample_file ${simsFolder}/simulated_mg.fq.sig --outfile ${simsFolder}/EU_results_default.csv
+python ../../recover_abundance.py --ref_file default_EU_ref_matrix_processed.npz  --ksize 31 --sample_file simulated_mg.fq.sig --outfile EU_results_default.csv
 
-numUnknownReads=$(grep -v -f ${simsFolder}/known_names.txt ${simsFolder}/simulation_counts.csv | cut -d',' -f2 | awk '{SUM+=$1}END{print SUM}')
+#numUnknownReads=$(grep -v -f known_names.txt simulation_counts.csv | cut -d',' -f2 | awk '{SUM+=$1}END{print SUM}')
+numUnknownReads=$(cat unknown_names.txt | xargs -I{} grep {} simulation_counts.csv | cut -d',' -f2 | awk '{SUM+=$1}END{print SUM}')
 unknownPercent=$(echo "scale=8; ${numUnknownReads} / ${numReads}" | bc)
-sourmashEstimateKnown=$(cut -d',' -f 5 ${simsFolder}/gather_results.csv | tail -n +2 | awk '{SUM+=$1}END{print SUM}')
+sourmashEstimateKnown=$(cut -d',' -f 5 gather_results.csv | tail -n +2 | awk '{SUM+=$1}END{print SUM}')
 sourmashEstimateUnknown=$(echo "scale=8; 1 - ${sourmashEstimateKnown}" | bc)
-ourEstimate=$(sed -n 2p ${simsFolder}/EU_results_default.csv | rev | cut -d',' -f1 | rev)
-echo "true_unknown,sourmash_estimate,our_estimate" > ${simsFolder}/results.txt
-echo "${unknownPercent},${sourmashEstimateUnknown},${ourEstimate}" >> ${simsFolder}/results.txt
-echo "${unknownPercent},${sourmashEstimateUnknown},${ourEstimate}" >> all_results.txt
-rm ${simsFolder}/simulated_mg.fq
+ourEstimate=$(sed -n 2p EU_results_default.csv | rev | cut -d',' -f1 | rev)
+echo "true_unknown,sourmash_estimate,our_estimate,num_reads" > results.txt
+echo "${unknownPercent},${sourmashEstimateUnknown},${ourEstimate},${numReads}" >> results.txt
+echo "${unknownPercent},${sourmashEstimateUnknown},${ourEstimate},${numReads}" >> ../all_results.txt
