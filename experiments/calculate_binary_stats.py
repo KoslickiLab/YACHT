e72c1da4efa70c31dab3d1bc18063ca42decadec
David Koslicki <dmkoslicki@gmail.com>
2022-11-28 12:59:53 -0500

    CLI for #5

--- experiments/calculate_binary_stats.py
+++ experiments/calculate_binary_stats.py
@@ -4,79 +4,90 @@ import os
 from os.path import exists
 import argparse
 
-sims_dir = '.'
-actual_unknowns_file = os.path.join(sims_dir, 'actual_unknowns.txt')
-gather_results_file = os.path.join(sims_dir, 'gather_results.csv')
-our_results_file = os.path.join(sims_dir, 'EU_results_default.csv')
-if not exists(actual_unknowns_file):
-    raise FileNotFoundError(f'Could not find {actual_unknowns_file}. Invoke this script from where the file resides.')
-if not exists(gather_results_file):
-    raise FileNotFoundError(f'Could not find {gather_results_file}. Invoke this script from where the file resides.')
-if not exists(our_results_file):
-    raise FileNotFoundError(f'Could not find {our_results_file}. Invoke this script from where the file resides.')
 
-# Get the actual unknowns
-actual_unknowns = []
-with open(actual_unknowns_file, 'r') as f:
-    for line in f.readlines():
-        actual_unknowns.append(line.strip())
-actual_unknowns = set(actual_unknowns)
+def main():
+    # single argument: directory containing the results of the simulation
+    parser = argparse.ArgumentParser(description='Calculate the binary stats for both sourmash and our approach')
+    parser.add_argument('-d', '--dir', help='Directory containing the unknown and known signatures', required=True)
+    args = parser.parse_args()
+    sims_dir = args.dir
 
-# get the sourmash results
-sourmash_name_loc = 9
-sourmash_results = []
-with open(gather_results_file, 'r') as f:
-    # skip the header
-    f.readline()
-    for line in f.readlines():
-        name = line.strip().split(',')[sourmash_name_loc]
-        name = name.split('.')[0]
-        sourmash_results.append(name)
-sourmash_results = set(sourmash_results)
+    #sims_dir = '.'
+    actual_unknowns_file = os.path.join(sims_dir, 'actual_unknowns.txt')
+    gather_results_file = os.path.join(sims_dir, 'gather_results.csv')
+    our_results_file = os.path.join(sims_dir, 'EU_results_default.csv')
+    if not exists(actual_unknowns_file):
+        raise FileNotFoundError(f'Could not find {actual_unknowns_file}. Invoke this script from where the file resides.')
+    if not exists(gather_results_file):
+        raise FileNotFoundError(f'Could not find {gather_results_file}. Invoke this script from where the file resides.')
+    if not exists(our_results_file):
+        raise FileNotFoundError(f'Could not find {our_results_file}. Invoke this script from where the file resides.')
 
-# get our results
-our_results = []
-our_name_loc = 1
-rel_abund_loc = 13
-with open(our_results_file, 'r') as f:
-    # skip the header
-    f.readline()
-    for line in f.readlines():
-        name = line.strip().split(',')[our_name_loc]
-        name = name.split('.')[0]
-        rel_abund = float(line.strip().split(',')[rel_abund_loc])
-        if rel_abund > 0:
-            our_results.append(name)
-our_results = set(our_results)
+    # Get the actual unknowns
+    actual_unknowns = []
+    with open(actual_unknowns_file, 'r') as f:
+        for line in f.readlines():
+            actual_unknowns.append(line.strip())
+    actual_unknowns = set(actual_unknowns)
 
-# Import the thought to be knowns
-ground_truths = []
-with open(os.path.join(sims_dir, 'known_names.txt'), 'r') as f:
-    for line in f.readlines():
-        line = line.strip().split('.')[0]
-        ground_truths.append(line)
-ground_truths = set(ground_truths)
+    # get the sourmash results
+    sourmash_name_loc = 9
+    sourmash_results = []
+    with open(gather_results_file, 'r') as f:
+        # skip the header
+        f.readline()
+        for line in f.readlines():
+            name = line.strip().split(',')[sourmash_name_loc]
+            name = name.split('.')[0]
+            sourmash_results.append(name)
+    sourmash_results = set(sourmash_results)
 
-# remove the unknowns from the ground truths
-ground_truths = ground_truths - actual_unknowns
+    # get our results
+    our_results = []
+    our_name_loc = 1
+    rel_abund_loc = 13
+    with open(our_results_file, 'r') as f:
+        # skip the header
+        f.readline()
+        for line in f.readlines():
+            name = line.strip().split(',')[our_name_loc]
+            name = name.split('.')[0]
+            rel_abund = float(line.strip().split(',')[rel_abund_loc])
+            if rel_abund > 0:
+                our_results.append(name)
+    our_results = set(our_results)
 
-# calculate TP, FP, and FN for sourmash and our approach
-TP_sourmash = len(ground_truths.intersection(sourmash_results))
-FP_sourmash = len(sourmash_results - ground_truths)
-FN_sourmash = len(ground_truths - sourmash_results)
+    # Import the thought to be knowns
+    ground_truths = []
+    with open(os.path.join(sims_dir, 'known_names.txt'), 'r') as f:
+        for line in f.readlines():
+            line = line.strip().split('.')[0]
+            ground_truths.append(line)
+    ground_truths = set(ground_truths)
 
-TP_our = len(ground_truths.intersection(our_results))
-FP_our = len(our_results - ground_truths)
-FN_our = len(ground_truths - our_results)
+    # remove the unknowns from the ground truths
+    ground_truths = ground_truths - actual_unknowns
 
-# print the results
-print("Method, TP, FP, FN")
-print(f"sourmash, {TP_sourmash}, {FP_sourmash}, {FN_sourmash}")
-print(f"ours, {TP_our}, {FP_our}, {FN_our}")
+    # calculate TP, FP, and FN for sourmash and our approach
+    TP_sourmash = len(ground_truths.intersection(sourmash_results))
+    FP_sourmash = len(sourmash_results - ground_truths)
+    FN_sourmash = len(ground_truths - sourmash_results)
 
-# save the results to a file
-with open(os.path.join(sims_dir, 'binary_stats.csv'), 'w') as f:
-    f.write("Method,TP,FP,FN\n")
-    f.write(f"sourmash,{TP_sourmash},{FP_sourmash},{FN_sourmash}\n")
-    f.write(f"ours,{TP_our},{FP_our},{FN_our}\n")
+    TP_our = len(ground_truths.intersection(our_results))
+    FP_our = len(our_results - ground_truths)
+    FN_our = len(ground_truths - our_results)
 
+    # print the results
+    print("Method, TP, FP, FN")
+    print(f"sourmash, {TP_sourmash}, {FP_sourmash}, {FN_sourmash}")
+    print(f"ours, {TP_our}, {FP_our}, {FN_our}")
+
+    # save the results to a file
+    with open(os.path.join(sims_dir, 'binary_stats.csv'), 'w') as f:
+        f.write("Method,TP,FP,FN\n")
+        f.write(f"sourmash,{TP_sourmash},{FP_sourmash},{FN_sourmash}\n")
+        f.write(f"ours,{TP_our},{FP_our},{FN_our}\n")
+
+
+if __name__ == '__main__':
+    main()
