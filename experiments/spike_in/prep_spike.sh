55c86390b931417e59c8be7029a68cbc1d7223fc
dkoslicki <dmkoslicki@gmail.com>
2022-12-01 22:07:55 -0500

    make it easier to do replicates of the spike in #5

--- experiments/spike_in/prep_spike.sh
+++ experiments/spike_in/prep_spike.sh
@@ -22,8 +22,10 @@ set -o pipefail
 # get the names of the undetected organisms
 # grep -v -f detected_orgs.txt ../MANIFEST.csv | cut -d',' -f 10 > absent_names.txt
 
+iter=1
+coverageValues=(".5" ".25" ".125" ".0625" ".03125" ".015625" ".0078125" ".00390625" ".001953125" ".0009765625") 
 # For commenting out stuff I've already done
-: <<'END'
+#: <<'END'
 # Randomly pull one of these out
 cat absent_names.txt | shuf | head -n1 > spike_in_name.txt
 
@@ -33,46 +35,41 @@ cp ${spikeName} to_spike_in.fna
 
 # Subsample to various coverage amounts
 # This assumes that the repo KEGG_sketching_annotation is adjacent to Estimating_Unknowns
-../../../KEGG_sketching_annotation/utils/bbmap/./randomreads.sh ref=to_spike_in.fna out=coverage_.5X.fna coverage=0.5
-../../../KEGG_sketching_annotation/utils/bbmap/./randomreads.sh ref=to_spike_in.fna out=coverage_.25X.fna coverage=0.25
-../../../KEGG_sketching_annotation/utils/bbmap/./randomreads.sh ref=to_spike_in.fna out=coverage_.125X.fna coverage=0.125
-../../../KEGG_sketching_annotation/utils/bbmap/./randomreads.sh ref=to_spike_in.fna out=coverage_.0625X.fna coverage=0.0625
-../../../KEGG_sketching_annotation/utils/bbmap/./randomreads.sh ref=to_spike_in.fna out=coverage_.03125X.fna coverage=0.03125
+for cov in ${coverageValues[@]}
+do
+	../../../KEGG_sketching_annotation/utils/bbmap/./randomreads.sh ref=to_spike_in.fna overwrite=t out=coverage_${cov}X.fna coverage=0${cov}
+done
 
 # Sketch all of these
-for x in ".5" ".25" ".125" ".0625" ".03125"
+for cov in ${coverageValues[@]}
 do 
-	sourmash sketch dna -f -p k=31,scaled=1000,abund -o coverage_${x}X.fna.sig coverage_${x}X.fna
+	sourmash sketch dna -f -p k=31,scaled=1000,abund -o coverage_${cov}X.fna.sig coverage_${cov}X.fna
 done
 
 # Merge these into the sketch of the real metagenome
-for x in ".5" ".25" ".125" ".0625" ".03125"
+for cov in ${coverageValues[@]}
 do 
-	sourmash sig merge -o merged_${x}X.sig coverage_${x}X.fna.sig 36116.SZAXPI030664-33.clean.trim.rmhost.1.fq.sig
-done
-END
-# Run our abundance recovery
-for sampleCov in ".5" ".25" ".125" ".0625" ".03125" 
-do
-	for covThresh in ".5" ".25" ".125" ".0625" ".03125"
-	do
-		python ../../recover_abundance.py --ref_file ../formatted_db_ref_matrix_processed.npz --ksize 31 --sample_file merged_${sampleCov}X.sig --outfile EU_results_sample_cov_${sampleCov}X_cov_thresh_${covThresh}X.csv --min_coverage ${covThresh}
-	done
+	sourmash sig merge -o merged_${cov}X.sig coverage_${cov}X.fna.sig 36116.SZAXPI030664-33.clean.trim.rmhost.1.fq.sig
 done
 
-# Get the results
-for sampleCov in ".5" ".25" ".125" ".0625" ".03125"
+# Run our abundance recovery
+for sampleCov in ${coverageValues[@]} 
 do
-        for covThresh in ".5" ".25" ".125" ".0625" ".03125"
+	for covThresh in ${coverageValues[@]}
+	do
+		python ../../recover_abundance.py --ref_file ../formatted_db_ref_matrix_processed.npz --ksize 31 --sample_file merged_${sampleCov}X.sig --outfile EU_results_sample_cov_${sampleCov}X_cov_thresh_${covThresh}X.csv --min_coverage ${covThresh} &
+	done
+done
+#END
+# Prep the output CSV file
+echo "sample_coverage, coverage_threshold, recovered_kmer_abundance" > results_iter_${iter}.csv
+
+# Get the results
+for sampleCov in ${coverageValues[@]}
+do
+        for covThresh in ${coverageValues[@]}
         do
-		relAb=$(grep -f spike_in_name.txt EU_results_sample_cov_${sampleCov}X_cov_thresh_${covThresh}X.csv | cut -d',' -f14)
-		echo "Sample_Cov,${sampleCov}"
-                echo "Cov_Thresh,${covThresh}"
-		if [ -z $relAb]
-		then
-			echo $relAb
-		else
-			echo 0
-		fi
+		relAb=$(grep -f spike_in_name.txt EU_results_sample_cov_${sampleCov}X_cov_thresh_${covThresh}X.csv | cut -d',' -f17)
+		echo "${sampleCov}, ${covThresh}, ${relAb}" >> results_iter_${iter}.csv"
 	done
 done
