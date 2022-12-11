1331a7de59d7b21f4dc80e5f3993406e44a268a7
dkoslicki <dmkoslicki@gmail.com>
2022-12-10 20:58:44 -0500

    also vary mutation threshold #5

--- experiments/mut_thresh_spike_and_cov/run_recovery.sh
+++ experiments/mut_thresh_spike_and_cov/run_recovery.sh
@@ -4,16 +4,20 @@ set -u
 set -o pipefail
 
 # Run our abundance recovery
+mutThreshs=("0.001" "0.005" "0.01" "0.02" "0.05" "0.1" "0.15")
 coverageValues=(".25" ".0625" ".015625" ".00390625" ".0009765625")
-for spikeCov in ${coverageValues[@]}
+for mut in mutThreshs
 do
-        for covThresh in ${coverageValues[@]}
+	for spikeCov in ${coverageValues[@]}
 	do
-		for line in `tail -n +2 sigs_md5_to_accession_to_gtdb_location.txt | head -n 10`
-	        do
-               		md5short=$(echo ${line} | cut -d',' -f2)
-                	echo "python ../../recover_abundance.py --ref_file ../formatted_db_ref_matrix_processed.npz --ksize 31 --sample_file spikes_cov_${spikeCov}/${md5short}_spiked_sample.sig --outfile EU_on_spikes_cov_${spikeCov}/${md5short}_${spikeCov}X_cov_thresh_${covThresh}X.csv --min_coverage ${covThresh}"
-	        done | parallel -j 50
+        	for covThresh in ${coverageValues[@]}
+		do
+			for line in `tail -n +2 sigs_md5_to_accession_to_gtdb_location.txt | head -n 10`
+	        	do
+               			md5short=$(echo ${line} | cut -d',' -f2)
+                		echo "python ../../recover_abundance.py --ref_file ../formatted_db_ref_matrix_processed.npz --ksize 31 --sample_file spikes_cov_${spikeCov}/${md5short}_spiked_sample.sig --outfile EU_on_spikes_cov_${spikeCov}/${md5short}_${spikeCov}X_cov_thresh_${covThresh}X_mut_thresh_${mut}.csv --min_coverage ${covThresh}" --mut_thresh ${mut}
+	        	done | parallel -j 50
+		done
 	done
 done
 
