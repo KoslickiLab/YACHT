16545ac3f4ac5a76908fecb3f1648f3d7f1c3714
dkoslicki <dmkoslicki@gmail.com>
2022-12-13 23:01:19 -0500

    more changes for #7

---
+++ experiments/run_a_bunch_hyp.sh
@@ -0,0 +1,3 @@
+ for i in `seq 5`; do sleep 10; ./end_to_end_full_ref_hyp.sh & done
+ for i in `seq 5`; do sleep 10; ./end_to_end_full_ref_hyp.sh & done
+ for i in `seq 5`; do sleep 10; ./end_to_end_full_ref_hyp.sh & done
