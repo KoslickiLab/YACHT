ed6911c0ad8d37320025c23c8b8a967dd76ca1e7
dkoslicki <dmkoslicki@gmail.com>
2022-12-22 21:16:21 -0500

    a redone approach to #7 after the dictionary formation was fixed

---
+++ experiments/mut_thresh_spike_and_cov_redo/run_recovery_hyp.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -e
+set -u
+set -o pipefail
+
+# Run our abundance recovery
+mutThreshs=("0.001" "0.01" "0.05" "0.1")
+minMut=0.001
+coverageValues=("1" "0.1" "0.01" "0.001")
+for mut in "${mutThreshs[@]}"
+do
+	for spikeCov in "${coverageValues[@]}"
+	do
+		mkdir -p EU_on_spikes_cov_${spikeCov}_hyp
+        	for covThresh in "${coverageValues[@]}"
+		do
+			for line in `tail -n +2 sigs_md5_to_accession_to_gtdb_location_mut_${mut}.txt`
+	        	do
+               			md5short=$(echo ${line} | cut -d',' -f2)
+                		echo "python ../../recover_abundance.py --ref_file ../formatted_db_mut_${mut}_ref_matrix_processed.npz --ksize 31 --sample_file spikes_cov_${spikeCov}/${md5short}_spiked_sample.sig.zip --outfile EU_on_spikes_cov_${spikeCov}_hyp/${md5short}_${spikeCov}X_cov_thresh_${covThresh}X_mut_thresh_${mut}_hyp.csv --min_coverage ${covThresh}" --mut_thresh ${mut} --recovery_method h
+	        	done | parallel -j 200
+		done
+	done
+done
+
+
