05e0fae6e84f7dbb48d8dc4403a1d9ac14323e97
dkoslicki <dmkoslicki@gmail.com>
2023-04-29 14:23:39 -0400

    add more unit tests for #10

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,4 +1,5 @@
 # Unit tests
+import numpy as np
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
@@ -6,4 +7,12 @@ from hypothesis_recovery import get_alt_mut_rate
 
 
 def test_get_alt_mut_rate_1():
+    # All these are calculated with Steve's code
     assert get_alt_mut_rate(100, 10000, 21, significance=0.99) == -1
+    assert np.isclose(get_alt_mut_rate(10, 0, 21), 0.28015945851802826)
+    assert np.isclose(get_alt_mut_rate(10, 0, 31), 0.19963312102481723)
+    assert np.isclose(get_alt_mut_rate(10, 5, 21), 0.0698992155957967)
+    assert np.isclose(get_alt_mut_rate(10, 5, 31), 0.047902071848511696)
+    assert np.isclose(get_alt_mut_rate(10, 9, 21), 0.02169068099465221)
+    assert np.isclose(get_alt_mut_rate(100, 10, 11), 0.2397729973308742)
+    assert np.isclose(get_alt_mut_rate(1000, 0, 1), 0.9999899497147453)
