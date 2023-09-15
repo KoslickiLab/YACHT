d891933a5786ba14600ceb7e914aaec5daa43ad7
David Koslicki <dmkoslicki@gmail.com>
2023-09-15 15:42:40 -0400

    Add test for #5 and YAC-1

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -33,6 +33,7 @@ def test_load_signature_with_ksize1():
     # can we do a simple operation on it?
     assert sig.jaccard(sig) == 1.0
 
+
 def test_load_signature_with_ksize2():
     # wrong k-size
     file = to_testing_data("sample.sig")
@@ -46,6 +47,23 @@ def test_load_signature_with_ksize2():
         sig = utils.load_signature_with_ksize(file, 31)
     except ValueError:
         pass
+    # too many files
+    try:
+        sig = utils.load_signature_with_ksize(to_testing_data("20_genomes_sketches.zip"), 31)
+    except ValueError:
+        pass
+
+
+def test_load_signature_with_ksize3():
+    # different kind of format
+    file = to_testing_data("sample.sig")
+    sig = utils.load_signature_with_ksize(file, 31)
+    sourmash.save_signatures([sig], open(to_testing_data('test.sig.zip'), 'wb'), compression=1)
+    sig = utils.load_signature_with_ksize(to_testing_data('test.sig.zip'), 31)
+    assert type(sig) == sourmash.signature.FrozenSourmashSignature
+    assert sig.jaccard(sig) == 1.0
+
+
 
 
 
