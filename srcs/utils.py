08ea0d5df9a309095180cfc302a629e2f261977a
Chunyu Ma <machunyu4402@hotmail.com>
2023-09-22 11:37:29 -0400

    modified a few functions and class from opal and added them for BIOM format output

--- srcs/utils.py
+++ srcs/utils.py
@@ -1,4 +1,4 @@
-import os
+import os, sys
 import numpy as np
 import pickle
 import sourmash
@@ -6,7 +6,9 @@ from tqdm import tqdm, trange
 import scipy as sp 
 import csv
 import zipfile
-
+from loguru import logger
+logger.remove()
+logger.add(sys.stdout, format="{time:YYYY-MM-DD HH:mm:ss} - {level} - {message}", level="INFO")
 
 def load_hashes(filename):
     """
@@ -211,3 +213,176 @@ def write_processed_indices(filename, signatures, uncorr_org_idx):
         writer.writerow(['organism_name', 'original_index', 'processed_index', 'num_unique_kmers_in_genome_sketch', 'num_total_kmers_in_genome_sketch', 'genome_scale_factor'])
         for i, idx in enumerate(tqdm(uncorr_org_idx)):
             writer.writerow([signatures[idx].name, idx, i, len(signatures[idx].minhash.hashes), get_num_kmers(signatures[idx], scale=False), signatures[idx].minhash.scaled])
+
+class Prediction:
+    """
+    (thanks to https://github.com/CAMI-challenge/OPAL, this class is from its load_data.py)
+    """
+    
+    def __init__(self):
+        pass
+
+    @property
+    def rank(self):
+        return self.__rank
+
+    @property
+    def taxid(self):
+        return self.__taxid
+
+    @property
+    def percentage(self):
+        return self.__percentage
+
+    @property
+    def taxpath(self):
+        return self.__taxpath
+
+    @property
+    def taxpathsn(self):
+        return self.__taxpathsn
+
+    @rank.setter
+    def rank(self, rank):
+        self.__rank = rank
+
+    @taxid.setter
+    def taxid(self, taxid):
+        self.__taxid = taxid
+
+    @percentage.setter
+    def percentage(self, percentage):
+        self.__percentage = percentage
+
+    @taxpath.setter
+    def taxpath(self, taxpath):
+        self.__taxpath = taxpath
+
+    @taxpathsn.setter
+    def taxpathsn(self, taxpathsn):
+        self.__taxpathsn = taxpathsn
+
+    def get_dict(self):
+        return self.__dict__
+
+    def get_pretty_dict(self):
+        return {property.split("_")[3]: value for property, value in self.__dict__.items()}
+
+    def get_metadata(self):
+        return {'rank': self.__rank, 'taxpath': self.__taxpath, 'taxpathsn': self.__taxpathsn}
+
+
+def get_column_indices(column_name_to_index):
+    """
+    (thanks to https://github.com/CAMI-challenge/OPAL, this function is modified get_column_indices from its load_data.py)
+    Helper function that gets the column indices for the following columns: TAXID, RANK, PERCENTAGE, TAXPATH, TAXPATHSN
+    :param column_name_to_index: dictionary mapping column name to column index
+    :return: indices for TAXID, RANK, PERCENTAGE, TAXPATH, TAXPATHSN
+    """
+    
+    if "TAXID" not in column_name_to_index:
+        logger.error("Column not found: {}".format("TAXID"))
+        raise RuntimeError
+    if "RANK" not in column_name_to_index:
+        logger.error("Column not found: {}".format("RANK"))
+        raise RuntimeError
+    if "PERCENTAGE" not in column_name_to_index:
+        logger.error("Column not found: {}".format("PERCENTAGE"))
+        raise RuntimeError
+    if "TAXPATH" not in column_name_to_index:
+        logger.error("Column not found: {}".format("TAXPATH"))
+        raise RuntimeError
+    index_taxid = column_name_to_index["TAXID"]
+    index_rank = column_name_to_index["RANK"]
+    index_percentage = column_name_to_index["PERCENTAGE"]
+    index_taxpath = column_name_to_index["TAXPATH"]
+    if "TAXPATHSN" in column_name_to_index:
+        index_taxpathsn = column_name_to_index["TAXPATHSN"]
+    else:
+        index_taxpathsn = None
+    return index_rank, index_taxid, index_percentage, index_taxpath, index_taxpathsn
+
+def open_profile_from_tsv(cami_content):
+    """
+    (thanks to https://github.com/CAMI-challenge/OPAL, this function is modified open_profile_from_tsv from its load_data.py)
+    Helper function that opens a CAMI profile file and returns sample profiling information.
+    params:cami_content: list of strings (lines of the CAMI profile file)
+    return: list of tuples (sample_id, header, profile)
+    """
+    header = {}
+    column_name_to_index = {}
+    profile = []
+    samples_list = []
+    predictions_dict = {}
+    reading_data = False
+    got_column_indices = False
+
+    for line in cami_content:
+        if len(line.strip()) == 0 or line.startswith("#"):
+            continue
+        line = line.rstrip('\n')
+
+        # parse header with column indices
+        if line.startswith("@@"):
+            for index, column_name in enumerate(line[2:].split('\t')):
+                column_name_to_index[column_name] = index
+            index_rank, index_taxid, index_percentage, index_taxpath, index_taxpathsn = get_column_indices(column_name_to_index)
+            got_column_indices = True
+            reading_data = False
+            continue
+
+        # parse header with metadata
+        if line.startswith("@"):
+            # if last line contained sample data and new header starts, store profile for sample
+            if reading_data:
+                if 'SAMPLEID' in header and 'VERSION' in header and 'RANKS' in header:
+                    if len(profile) > 0:
+                        samples_list.append((header['SAMPLEID'], header, profile))
+                        profile = []
+                        predictions_dict = {}
+                else:
+                    logger.error("Header in file {} is incomplete. Check if the header of each sample contains at least SAMPLEID, VERSION, and RANKS.\n".format(file_path))
+                    raise RuntimeError
+                header = {}
+            reading_data = False
+            got_column_indices = False
+            key, value = line[1:].split(':', 1)
+            header[key.upper()] = value.strip()
+            continue
+
+        if not got_column_indices:
+            logger.error("Header line starting with @@ in file {} is missing or at wrong position.\n".format(file_path))
+            raise RuntimeError
+
+        reading_data = True
+        row_data = line.split('\t')
+
+        taxid = row_data[index_taxid]
+        # if there is already a prediction for taxon, only sum abundance
+        if taxid in predictions_dict:
+            prediction = predictions_dict[taxid]
+            prediction.percentage += float(row_data[index_percentage])
+        else:
+            if float(row_data[index_percentage]) == .0:
+                continue
+            prediction = Prediction()
+            predictions_dict[taxid] = prediction
+            prediction.taxid = row_data[index_taxid]
+            prediction.rank = row_data[index_rank]
+            prediction.percentage = float(row_data[index_percentage])
+            prediction.taxpath = row_data[index_taxpath]
+            if isinstance(index_taxpathsn, int):
+                prediction.taxpathsn = row_data[index_taxpathsn]
+            else:
+                prediction.taxpathsn = None
+            profile.append(prediction)
+
+    # store profile for last sample
+    if 'SAMPLEID' in header and 'VERSION' in header and 'RANKS' in header:
+        if reading_data and len(profile) > 0:
+            samples_list.append((header['SAMPLEID'], header, profile))
+    else:
+        logger.error("Header in file {} is incomplete. Check if the header of each sample contains at least SAMPLEID, VERSION, and RANKS.\n".format(file_path))
+        raise RuntimeError
+
+    return samples_list
