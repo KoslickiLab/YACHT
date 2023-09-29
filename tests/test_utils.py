90871a756b92c7d70b45eafebed1c99e969ea1de
Max Lupei <mfl5855@psu.edu>
2023-09-29 18:32:06 -0400

    fixing tests, deleting unused files

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -11,17 +11,17 @@ import sourmash
 
 
 def to_testing_data(file):
-    return os.path.join("testdata", file)
+    return os.path.join('tests', os.path.join("testdata", file))
 
 
-def test_load_hashes():
-    # the *hash_to_col_idx.pkl files contain a pickle list of key value pairs with keys the hash values and values
-    # the index of the row that they appear in the npz matrix
-    file = to_testing_data("integration_test_hash_to_col_idx.pkl")
-    hashes = utils.load_hashes_to_index(file)
-    assert type(hashes) == dict
-    assert len(hashes) == 63888
-    assert np.allclose(np.sort(list(hashes.values())), range(0, len(hashes)))
+def test_load_hashes_to_index():
+     # the *hash_to_col_idx.pkl files contain a pickle list of key value pairs with keys the hash values and values
+     # the index of the row that they appear in the npz matrix
+     file = to_testing_data("integration_test_hash_to_col_idx.pkl")
+     hashes = utils.load_hashes_to_index(file)
+     assert type(hashes) == dict
+     assert len(hashes) == 63888
+     assert np.allclose(np.sort(list(hashes.values())), range(0, len(hashes)))
 
 
 def test_load_signature_with_ksize1():
