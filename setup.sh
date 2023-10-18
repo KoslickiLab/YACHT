27d29816ad7538d9a2d1cb0689f2fed914e43bfa
Chunyu Ma <machunyu4402@hotmail.com>
2023-10-18 12:35:07 -0400

    try to fix a bug in #36

--- setup.sh
+++ setup.sh
@@ -3,14 +3,14 @@
 
 # Check if the YACHT environment exists
 ENV_NAME="yacht_env"
-match_env=$(conda info --envs | grep -w $ENV_NAME)
+check=$(conda env list | cut -d" " -f 1 | grep -w $ENV_NAME | wc -l)
 
-if [ ! -z "$match_env" ]; then
+if [ $check -eq 1 ]; then
     echo "The environment '$ENV_NAME' already exists. Please activate it by running 'conda activate $ENV_NAME'."
 else
     conda env create -f env/yacht_env.yml
     
-    if [ ! -z "$match_env" ]; then
+    if [ $check -eq 1 ]; then
         echo "The environment '$ENV_NAME' has been successfully created and please activate it by running 'conda activate $ENV_NAME'."
     else
         echo "There was a problem creating the environment '$ENV_NAME'. Please check the error messages above."
