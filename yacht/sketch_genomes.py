e4748df48985e747642b531a59a300cb1068b565
Andreas Sjödin <andreas.sjodin@gmail.com>
2023-11-26 21:15:05 +0100

    Bug fixes

--- yacht/sketch_genomes.py
+++ yacht/sketch_genomes.py
@@ -2,21 +2,43 @@
 import argparse
 import subprocess
 import os
+import sys
 from pathlib import Path
-
+from loguru import logger
 from yacht import __version__
+
+logger.remove()
+logger.add(sys.stdout, format="{time:YYYY-MM-DD HH:mm:ss} - {level} - {message}", level="INFO")
+
 
 def sketch_single_file(infile, kmer, scaled, outfile):
     cmd = f"sourmash sketch dna -f -p k={kmer},scaled={scaled},abund --singleton {infile} -o {outfile}"
-    subprocess.run(cmd, shell=True, check=True)
+    try:
+        logger.info(f"Starting sketching of single file: {infile}")
+        subprocess.run(cmd, shell=True, check=True)
+        logger.success(f"Successfully sketched: {infile}")
+    except subprocess.CalledProcessError as e:
+        logger.error(f"Error occurred while sketching {infile}: {e}")
 
 def sketch_multiple_files(folder_path, kmer, scaled, outfile):
     dataset_file = os.path.join(folder_path, "dataset.csv")
-    with open(dataset_file, "w") as f:
-        for path in Path(folder_path).glob('**/*.fasta'):
-            f.write(f"{path}\n")
-    cmd = f"sourmash sketch fromfile {dataset_file} -p dna,k={kmer},scaled={scaled},abund -o {outfile}"
-    subprocess.run(cmd, shell=True, check=True)
+    file_extensions = ['*.fasta', '*.fna', '*.fas', '*.fa', '*.fasta.gz', '*.fna.gz', '*.fas.gz', '*.fa.gz']
+
+    try:
+        logger.info(f"Preparing dataset file for multiple sketching in folder: {folder_path}")
+        with open(dataset_file, "w") as f:
+            f.write("name,genome_filename,protein_filename\n")  # Add header for CSV
+            for extension in file_extensions:
+                for path in Path(folder_path).glob(f'**/{extension}'):
+                    name = path.stem  # Use the file name (without extension) as the sketch name
+                    f.write(f"{name},{path},\n")  # Write the name and the full file path
+
+        cmd = f"sourmash sketch fromfile {dataset_file} -p dna,k={kmer},scaled={scaled},abund -o {outfile}"
+        logger.info(f"Starting sketching of multiple files in: {folder_path}")
+        subprocess.run(cmd, shell=True, check=True)
+        logger.success(f"Successfully sketched files in: {folder_path}")
+    except subprocess.CalledProcessError as e:
+        logger.error(f"Error occurred while sketching files in {folder_path}: {e}")
 
 def main():
     parser = argparse.ArgumentParser(description="Sketch genomes using Sourmash.")
@@ -27,13 +49,15 @@ def main():
     parser.add_argument("--outfile", help="Output file name.", required=True)
     args = parser.parse_args()
 
-    # Check if input is a file or a directory
-    if os.path.isfile(args.infile):
-        sketch_single_file(args.infile, args.kmer, args.scaled, args.outfile)
-    elif os.path.isdir(args.infile):
-        sketch_multiple_files(args.infile, args.kmer, args.scaled, args.outfile)
-    else:
-        raise FileNotFoundError(f"Input path {args.infile} does not exist.")
+    try:
+        if os.path.isfile(args.infile):
+            sketch_single_file(args.infile, args.kmer, args.scaled, args.outfile)
+        elif os.path.isdir(args.infile):
+            sketch_multiple_files(args.infile, args.kmer, args.scaled, args.outfile)
+        else:
+            raise FileNotFoundError(f"Input path {args.infile} does not exist.")
+    except FileNotFoundError as e:
+        logger.error(e)
 
 if __name__ == "__main__":
     main()
