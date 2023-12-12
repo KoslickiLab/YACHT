f6cb0c96bb0a930b07d4f513e2d8c4585067ccce
Judith Rodriguez <judiths.r91@gmail.com>
2023-12-12 13:07:56 -0500

    fix bug yac 13

---
+++ tests/test_utils_for_bug_YAC-13.py
@@ -0,0 +1,20 @@
+import subprocess
+from os.path import exists
+import os
+import numpy as np
+import pandas as pd
+# add the parent directory to the path
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from yacht import utils
+import sourmash
+import pytest
+from  yacht.utils import load_signature_with_ksize
+
+def test_error_message_for_load_signature_with_ksize():
+     # Reference to YAC-13 bug on sketching short sequence/genomes
+     file = "tests/testdata_bug_YAC13/extract_empty_hash.sig.zip"
+     with pytest.raises(ValueError, match="Empty hash in signature. Please try sketch with '--scaled=1' or use alternative to sourmash."):
+        load_signature_with_ksize(file, 31)
+
+        
