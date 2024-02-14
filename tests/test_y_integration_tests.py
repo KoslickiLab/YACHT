dd6a8c125927a1ecfc40ea5c594fb4f6f8b18b5d
mfl15 <144844529+mfl15@users.noreply.github.com>
2024-02-14 12:45:53 -0500

    Update test_y_integration_tests.py

--- tests/test_y_integration_tests.py
+++ tests/test_y_integration_tests.py
@@ -76,11 +76,11 @@ def test_run_pretrained_ref_db():
     assert res.returncode == 0
 
 def test_run_yacht_pretrained_ref_db():
-    cmd = "yacht run --json ./gtdb-rs214-reps.k31_0.9995_pretrained/gtdb-rs214-reps.k31_0.9995_config.json --sample_file tests/testdata/sample.sig.zip --significance 0.99 --num_threads 32 --min_coverage_list 1 0.6 0.2 0.1 --out ./result.xlsx"
+    cmd = "yacht run --json ./gtdb-rs214-reps.k31_0.9995_pretrained/gtdb-rs214-reps.k31_0.9995_config.json --sample_file tests/testdata/sample.sig.zip --significance 0.99 --num_threads 32 --min_coverage_list 1 0.6 0.2 0.1 --out ./result_pretrained.xlsx"
     res = subprocess.run(cmd, shell=True, check=True)
     assert res.returncode == 0
 
-    assert exists('result.xlsx')
+    assert exists('result_pretrained.xlsx')
 
     
 
