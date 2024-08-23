2d28b636adfb853cae8343bb24cfe4accccccb1a
Chunyu Ma <machunyu4402@hotmail.com>
2024-08-23 12:29:50 -0400

    update packages

--- setup.py
+++ setup.py
@@ -1,11 +1,8 @@
 from setuptools import setup, find_packages
 
-# Import the version number
-from yacht import __version__
-
 setup(
     name='yacht',
-    version=__version__,
+    version='1.2.3',
     include_package_data=True,
     packages=find_packages(),
     entry_points={
@@ -14,6 +11,26 @@ setup(
         ],
     },
     python_requires='>=3.6',
+    # Include your pip-installable dependencies here
+    install_requires=[
+        'sourmash>=4.8.3,<5',
+        'scipy',
+        'numpy',
+        'pandas',
+        'scikit-learn',
+        'codecov',
+        'pytest',
+        'pytest-cov',
+        'loguru',
+        'requests',
+        'maturin>=1,<2',
+        'tqdm',
+        'biom-format',
+        'pytaxonkit',
+        'openpyxl',
+        'ruff',
+        'sourmash_plugin_branchwater'
+    ],
     # Add other package metadata here
     author='Koslicki, D., White, S., Ma, C., & Novikov, A.',
     description='YACHT is a mathematically rigorous hypothesis test for the presence or absence of organisms in a metagenomic sample, based on average nucleotide identity (ANI).',
