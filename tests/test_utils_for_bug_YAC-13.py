677b4703a6797934b3ecd568a1fcd1b7766ad69a
Chunyu Ma <machunyu4402@hotmail.com>
2024-11-04 13:29:20 -0500

    update tests code to fit the changes and fix a bug in tests

--- tests/test_utils_for_bug_YAC-13.py
+++ tests/test_utils_for_bug_YAC-13.py
@@ -5,7 +5,9 @@ import numpy as np
 import pandas as pd
 # add the parent directory to the path
 import sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+cpath = os.path.dirname(os.path.realpath(__file__))
+project_path = os.path.join(cpath,'..')
+sys.path.append(project_path)
 from yacht import utils
 import sourmash
 import pytest
@@ -13,7 +15,7 @@ from  yacht.utils import load_signature_with_ksize
 
 def test_error_message_for_load_signature_with_ksize():
      # Reference to YAC-13 bug on sketching short sequence/genomes
-     file = "tests/testdata_bug_YAC13/extract_empty_hash.sig.zip"
+     file = f"{project_path}/tests/testdata_bug_YAC13/extract_empty_hash.sig.zip"
      with pytest.raises(ValueError, match="Empty sketch in signature. This may be due to too high of a scale factor, please reduce it, eg. --scaled=1, and try again."):
         load_signature_with_ksize(file, 31)
 
