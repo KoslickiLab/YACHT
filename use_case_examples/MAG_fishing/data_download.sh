88f8d7ecb0fb3eaaea6736b7814c92ed7edc4bae
Judith Rodriguez <judiths.r91@gmail.com>
2025-02-21 17:32:20 -0500

    updated data_download file

--- use_case_examples/MAG_fishing/data_download.sh
+++ use_case_examples/MAG_fishing/data_download.sh
@@ -7,12 +7,6 @@ mkdir data
 fasterq-dump --concatenate-reads SRR32008482  -O data
 fasterq-dump --concatenate-reads SRR32008483  -O data
 
-### Download pre-reference signatures for k=31 and k=51
-#k-size=31
-wget https://farm.cse.ucdavis.edu/~ctbrown/sourmash-db/gtdb-rs214/gtdb-rs214-k31.zip --directory-prefix=data
-#k-size=51
-wget https://farm.cse.ucdavis.edu/~ctbrown/sourmash-db/gtdb-rs214/gtdb-rs214-k51.zip --directory-prefix=data
-
 ### Download MAG of interest
 datasets download genome accession GCA_017506175.1 --include genome --filename data/.
 cd data
