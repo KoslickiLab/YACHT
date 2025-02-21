e919f80abf83dd72cbea17bd3d640faa1c1019c8
Judith Rodriguez <judiths.r91@gmail.com>
2025-02-21 17:29:22 -0500

    updated data download script

--- use_case_examples/MAG_fishing/data_download.sh
+++ use_case_examples/MAG_fishing/data_download.sh
@@ -4,9 +4,17 @@
 mkdir data
 
 ### Download sample to data folder
-fasterq-dump --concatenate-reads ERR5161194 -O data
-fasterq-dump --concatenate-reads ERR10807863 -O data
+fasterq-dump --concatenate-reads SRR32008482  -O data
+fasterq-dump --concatenate-reads SRR32008483  -O data
 
-### Download reference
-datasets download genome accession GCF_016632365.1 --include genome --filename data/ncbi_dataset.zip
-unzip data/ncbi_dataset.zip
+### Download pre-reference signatures for k=31 and k=51
+#k-size=31
+wget https://farm.cse.ucdavis.edu/~ctbrown/sourmash-db/gtdb-rs214/gtdb-rs214-k31.zip --directory-prefix=data
+#k-size=51
+wget https://farm.cse.ucdavis.edu/~ctbrown/sourmash-db/gtdb-rs214/gtdb-rs214-k51.zip --directory-prefix=data
+
+### Download MAG of interest
+datasets download genome accession GCA_017506175.1 --include genome --filename data/.
+cd data
+unzip ncbi_dataset
+cd ../
